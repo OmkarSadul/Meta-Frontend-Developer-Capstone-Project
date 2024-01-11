90957098a39202951f5cae8c339463971fd657be
OmkarSadul <omkarsadul@gmail.com>
2024-01-11 21:10:39 +0530

    Icon/Images added and Header section created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,21 @@ import "./Header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="h-wrapper">
+        <div className="flexCenter paddings innerWidth h-container">
+            <img src="./logo.svg" alt="logo" width={170} />
+            <div className="flexCenter h-menu">
+                <a className='inside-menu' href="">Home</a>
+                <a className='inside-menu' href="">About</a>
+                <a className='inside-menu' href="">Menu</a>
+                <a className='inside-menu' href="">Reservation</a>
+                <a className='inside-menu' href="">Login</a>
+                <button className='button'>
+                    <a href="">Order Online</a>
+                </button>
+            </div>
+        </div>
+    </section>
   )
 }
 
